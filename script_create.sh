9a23efe085a5eefee3a45a34b35e7ba5e7a363af
Avinash445k <avinash0003041997@gmail.com>
2024-12-02 22:57:00 +0530

    upload file

---
+++ script_create.sh
@@ -0,0 +1,12 @@
+# hardware info check command 
+hwinfo 
+# hwinfo install command 
+sudo apt install hwinfo 
+# install for brave 
+sudo apt install brave 
+# install for tilix
+sudo apt install tilix 
+# install for vscode
+sudo apt install vscode 
+# install for docker desktop
+sudo apt install docker desktop 
